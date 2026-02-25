c0d52c4c5a5847084693784ab412a2f725d1d548
AkshatTripathi77 <gamingx123456@gmail.com>
2026-02-25 21:35:58 +0530

    Swap of 2 numbers

---
+++ Sawp.java
@@ -0,0 +1,12 @@
+public class Sawp {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a  + " " +b);
+
+
+    }
+}
